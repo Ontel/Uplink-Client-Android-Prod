cf3fe766029b3ce8bf1ff6dce78cc55e0f157b49
thefirstequinox <joaquin@ontel.co>
2024-10-15 00:40:28 +0800

    update(camera): Updated Cordova Camera

--- assets/www/plugins/cordova-plugin-camera/www/CameraConstants.js
+++ assets/www/plugins/cordova-plugin-camera/www/CameraConstants.js
@@ -27,25 +27,19 @@ module.exports = {
   /**
    * @description
    * Defines the output format of `Camera.getPicture` call.
-   * _Note:_ On iOS passing `DestinationType.NATIVE_URI` along with
-   * `PictureSourceType.PHOTOLIBRARY` or `PictureSourceType.SAVEDPHOTOALBUM` will
-   * disable any image modifications (resize, quality change, cropping, etc.) due
-   * to implementation specific.
    *
    * @enum {number}
    */
-  DestinationType:{
-    /** Return base64 encoded string. DATA_URL can be very memory intensive and cause app crashes or out of memory errors. Use FILE_URI or NATIVE_URI if possible */
+  DestinationType: {
+    /** Return base64 encoded string. DATA_URL can be very memory intensive and cause app crashes or out of memory errors. Use FILE_URI if possible */
     DATA_URL: 0,
     /** Return file uri (content://media/external/images/media/2 for Android) */
-    FILE_URI: 1,
-    /** Return native uri (eg. asset-library://... for iOS) */
-    NATIVE_URI: 2
+    FILE_URI: 1
   },
   /**
    * @enum {number}
    */
-  EncodingType:{
+  EncodingType: {
     /** Return JPEG encoded image */
     JPEG: 0,
     /** Return PNG encoded image */
@@ -54,50 +48,47 @@ module.exports = {
   /**
    * @enum {number}
    */
-  MediaType:{
+  MediaType: {
     /** Allow selection of still pictures only. DEFAULT. Will return format specified via DestinationType */
     PICTURE: 0,
     /** Allow selection of video only, ONLY RETURNS URL */
     VIDEO: 1,
     /** Allow selection from all media types */
-    ALLMEDIA : 2
+    ALLMEDIA: 2
   },
   /**
    * @description
    * Defines the output format of `Camera.getPicture` call.
-   * _Note:_ On iOS passing `PictureSourceType.PHOTOLIBRARY` or `PictureSourceType.SAVEDPHOTOALBUM`
-   * along with `DestinationType.NATIVE_URI` will disable any image modifications (resize, quality
-   * change, cropping, etc.) due to implementation specific.
    *
    * @enum {number}
    */
-  PictureSourceType:{
+  PictureSourceType: {
     /** Choose image from the device's photo library (same as SAVEDPHOTOALBUM for Android) */
-    PHOTOLIBRARY : 0,
+    PHOTOLIBRARY: 0,
     /** Take picture from camera */
-    CAMERA : 1,
+    CAMERA: 1,
     /** Choose image only from the device's Camera Roll album (same as PHOTOLIBRARY for Android) */
-    SAVEDPHOTOALBUM : 2
+    SAVEDPHOTOALBUM: 2
   },
   /**
    * Matches iOS UIPopoverArrowDirection constants to specify arrow location on popover.
    * @enum {number}
    */
-  PopoverArrowDirection:{
-      ARROW_UP : 1,
-      ARROW_DOWN : 2,
-      ARROW_LEFT : 4,
-      ARROW_RIGHT : 8,
-      ARROW_ANY : 15
+  PopoverArrowDirection: {
+    ARROW_UP: 1,
+    ARROW_DOWN: 2,
+    ARROW_LEFT: 4,
+    ARROW_RIGHT: 8,
+    ARROW_ANY: 15
   },
   /**
    * @enum {number}
    */
-  Direction:{
-      /** Use the back-facing camera */
-      BACK: 0,
-      /** Use the front-facing camera */
-      FRONT: 1
+  Direction: {
+    /** Use the back-facing camera */
+    BACK: 0,
+    /** Use the front-facing camera */
+    FRONT: 1
   }
 };
 

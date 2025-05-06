c314b48902f1d2616d4ca65fe36794203ffaeb56
thefirstequinox <joaquin@ontel.co>
2025-05-07 02:19:22 +0800

    update(build): Updated AGP and other misc. build tools

--- CordovaLib/src/org/apache/cordova/engine/SystemWebViewEngine.java
+++ CordovaLib/src/org/apache/cordova/engine/SystemWebViewEngine.java
@@ -210,9 +210,10 @@ public class SystemWebViewEngine implements CordovaWebViewEngine {
 
         // Enable AppCache
         // Fix for CB-2282
-        settings.setAppCacheMaxSize(5 * 1048576);
-        settings.setAppCachePath(databasePath);
-        settings.setAppCacheEnabled(true);
+//        settings.setAppCacheMaxSize(5 * 1048576);
+//        settings.setAppCachePath(databasePath);
+//        settings.setAppCacheEnabled(true);
+        settings.setCacheMode(WebSettings.LOAD_DEFAULT);
 
         // Fix for CB-1405
         // Google issue 4641
